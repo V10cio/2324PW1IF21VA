b8275d642a921bd6fef74e4cb33504bf7373d68c
V10cio <161033040+V10cio@users.noreply.github.com>
2024-04-05 16:43:26 +0700

    membuat form

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,12 @@
-document.getElementById("judul"),innerHTML = "JavaScript";
+document.getElementById("judul").innerHTML = "JavaScript";
+
+
+let desc = document.getElementById("deskripsi");
+desc.innerHTML = "Belajar DOM, variable, perulangan";
+
+let nama = document.getElementById("nama")
+let btnSimpan = document.getElementById("simpan");
+
+btnSimpan.addEventListener('click', function () {
+    console.log("Tombol ditekan");
+})
