df78b4e1d72038a4887c4fe9c21a7cfdcf5558bb
V10cio <161033040+V10cio@users.noreply.github.com>
2024-04-26 11:51:09 +0700

    index.html

---
+++ iseng/script.js
@@ -0,0 +1,32 @@
+.banner {
+    background: url('img/astronomy-1867616_1280.jpg') no-repeat center center;
+    background-size: cover;
+    padding-top: 10%;
+    padding-bottom: 10%;
+    color: antiquewhite;
+}
+
+.crop-img{
+    object-fit: cover;
+}
+
+.tim img{
+    width: 350px;
+    border: 15px;
+    border-radius: 5px;
+}
+
+a.social {
+    display: inline-block;
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #333;
+    line-height: 40px;
+}
+
+.kontak {
+    background-size: cover;
+    padding-top: 20%;
+    padding-bottom: 20%;
+}
